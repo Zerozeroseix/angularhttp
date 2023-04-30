5e072dede59b1f32d329f5e04255409dfcb37277
Zerozeroseix <mulomedicus@hotmail.com>
2023-04-30 20:27:35 +0200

    environment variables

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,21 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { User } from '../interface/user';
+import { environment } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-
+  private apiUrl = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
 
   getUser(userId: number): Observable<User> {
-    return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${userId.toString()}`)
+    return this.http.get<User>(`${this.apiUrl}/users/${userId.toString()}`)
   }
 }
